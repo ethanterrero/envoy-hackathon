8162b0ff81a36cebe346d2a6985373589470b220
Miguel <MiguelPimienta19@gmail.com>
2025-10-08 09:32:34 -0700

    initial setup with all dependiences

---
+++ daily-news/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2 className="text-2xl font-bold text-blue-500"> Daily News</h2>
+    </>
+  )
+}
+
+export default App
